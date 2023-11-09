df620475e0db8a398e0832dd02aa42c5d8e55d63
Chase Cummins <chase@Chases-MacBook-Pro.local>
2023-11-09 17:23:43 -0600

    Further refactors code

--- password_generator.py
+++ password_generator.py
@@ -1,7 +1,7 @@
 import secrets
 import numpy as np
 
-def calculate_entropy(numOptions, length):
+def calculate_bits_entropy(numOptions, length):
     """Calculates the entropy of a password
     
     Args:
@@ -14,7 +14,7 @@ def calculate_entropy(numOptions, length):
 
     return length * np.log2(numOptions)
 
-def calc_num_years_from_bits_entropy(bits_entropy):
+def calc_num_years_from_bits_entropy(bits_entropy, checks_per_second):
     """Calculates the number of years to bruteforce a password with bits_entropy on classical and quantum computers
 
     Args:
@@ -25,16 +25,17 @@ def calc_num_years_from_bits_entropy(bits_entropy):
         float: number of years on quantum
     """
     
-    CHECKS_PER_SEC = 10e18 # A quintillion checks per second (Fastest classical supercomputer) - Frontier
+    # Calculate years on classical
     classical_entropy = 2 ** bits_entropy
-    class_seconds = classical_entropy / CHECKS_PER_SEC
+    class_seconds = classical_entropy / checks_per_second
     class_minutes = class_seconds / 60
     class_hours = class_minutes / 60
     class_days = class_hours / 24
     class_years = class_days / 365
 
+    # Calculate years on quantum
     quantum_entropy = int(classical_entropy ** (1 / 2))
-    quant_seconds = quantum_entropy / CHECKS_PER_SEC
+    quant_seconds = quantum_entropy / checks_per_second
     quant_minutes = quant_seconds / 60
     quant_hours = quant_minutes / 60
     quant_days = quant_hours / 24
@@ -42,28 +43,42 @@ def calc_num_years_from_bits_entropy(bits_entropy):
 
     return class_years, quant_years
 
+
 if __name__ == "__main__":
     MIN_ENTROPY = 256
+    CHECKS_PER_SECOND = 10e18 # A quintillion checks per second (Fastest classical supercomputer) - Frontier
 
-    with open('/usr/share/dict/words') as f:
-        words = [word.strip() for word in f]
+    # Get the word list from a file
+    dictionary_file = "/usr/share/dict/words"
+    with open(dictionary_file) as file:
+        words = [word.strip() for word in file]
         num_options = len(words)
         print(f"The number of options is {num_options}")
 
-        num_words = 0
-        bits_entropy = 0
-        password = ''
-        while bits_entropy < MIN_ENTROPY:
-            password += secrets.choice(words) + " "
-            num_words += 1
-            bits_entropy = calculate_entropy(num_options, num_words)
+    num_words = 0
+    bits_entropy = 0
+    password = ''
 
-        password.strip()
-        bits_entropy = int(bits_entropy)
+    # Find a sufficiently complex password
+    while bits_entropy < MIN_ENTROPY:
+        password += secrets.choice(words) + " "
+        num_words += 1
+        bits_entropy = calculate_bits_entropy(num_options, num_words)
 
-        years = calc_num_years_from_bits_entropy(bits_entropy)
-        print(f"Years at {bits_entropy} bits entropy on classical computer: ", years[0],"\n"
-            "Years on quantum computer: ", years[1])
-        
-        print(f"The number of words is {num_words}")
-        print("The password is:\n", password)
+    # Strip extra whitespace
+    password.strip()
+
+    # Convert to an integer for clarity
+    bits_entropy = int(bits_entropy)
+
+    # Calculate the years
+    classical_years, quantum_years = calc_num_years_from_bits_entropy(bits_entropy, CHECKS_PER_SECOND)
+    print(f"Bits of entropy: {bits_entropy}")
+    print(f"Years on classical computer: {classical_years}")
+    print(f"Years on quantum computer: {quantum_years}")
+    
+    # Print the number of words and the generated password
+    print(f"The number of words is {num_words}")
+    print("#" * 30)
+    print(f"The password is:\n{password}")
+    print("#" * 30)
